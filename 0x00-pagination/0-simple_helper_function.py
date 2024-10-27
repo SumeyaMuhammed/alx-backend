61d39360cb156cd5ec74198f9b8b2e46a1955ea5
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-26 23:48:22 -1100

    reurn tuple

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ from typing import Tuple
     simple pagination py code
 '''
 
-def index_range(page, page_size) -> tuple[int, int]:
+def index_range(page, page_size) -> Tuple[int, int]:
     '''
     This function returns a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters.
     '''
